e2e385b9b3e44c6110df9a0bae09ebbe8bb47810
ht14495-ui <ht14495@gmail.com>
2026-06-10 15:57:51 +0530

    add homepage route

--- app.py
+++ app.py
@@ -12,6 +12,12 @@ def predict():
     df = pd.DataFrame([data])
     prediction = model.predict(df)[0]
     return jsonify({'predicted_price': round(float(prediction), 2)})
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({
+        'message': 'House Price Prediction API is running!',
+        'usage': 'Send a POST request to /predict with house features'
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
